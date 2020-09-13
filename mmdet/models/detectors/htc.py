7795e36419302c7094bf5a4fb6a7fef85c93ffa5
tianxinhang <54171139+tianxinhang@users.noreply.github.com>
2020-09-13 19:25:49 +0800

    Update htc.py

--- mmdet/models/detectors/htc.py
+++ mmdet/models/detectors/htc.py
@@ -13,3 +13,14 @@ class HybridTaskCascade(CascadeRCNN):
     def with_semantic(self):
         """bool: whether the detector has a semantic head"""
         return self.roi_head.with_semantic
+
+    def simple_test_2(self, img, img_metas, proposals=None, rescale=False):
+        """Test without augmentation."""
+        assert self.with_bbox, 'Bbox head must be implemented.'
+
+        x = self.extract_feat(img)
+        proposal_list = None
+        if proposals is not None:   # 我的方法就在这了。htc在预测阶段没有用semantic_pred。而我需要把这个利用起来。
+            proposal_list = proposals
+        tmp_rpn_head = self.rpn_head #惊险
+        return self.roi_head.simple_test_2(x, proposal_list, img_metas, tmp_rpn_head,rescale=rescale)
