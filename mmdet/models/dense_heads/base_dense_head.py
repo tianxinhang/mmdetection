b17ad3afbd6eea117e0dd340aa7407675be889e8
tianxinhang <54171139+tianxinhang@users.noreply.github.com>
2020-09-25 19:47:28 +0800

    Update base_dense_head.py

--- mmdet/models/dense_heads/base_dense_head.py
+++ mmdet/models/dense_heads/base_dense_head.py
@@ -18,6 +18,48 @@ class BaseDenseHead(nn.Module, metaclass=ABCMeta):
     def get_bboxes(self, **kwargs):
         """Transform network output for a batch into bbox predictions."""
         pass
+    
+#版本1.0 只改loss，且丢失的score不会在get box里被消除
+    def forward_train_2(self,
+                      x,
+                      img_metas,
+                      gt_bboxes,
+                        semantic_pred,
+                      gt_labels=None,
+                      gt_bboxes_ignore=None,
+                      proposal_cfg=None,
+                      **kwargs):
+        """
+        Args:
+            x (list[Tensor]): Features from FPN.
+            img_metas (list[dict]): Meta information of each image, e.g.,
+                image size, scaling factor, etc.
+            gt_bboxes (Tensor): Ground truth bboxes of the image,
+                shape (num_gts, 4).
+            gt_labels (Tensor): Ground truth labels of each box,
+                shape (num_gts,).
+            gt_bboxes_ignore (Tensor): Ground truth bboxes to be
+                ignored, shape (num_ignored_gts, 4).
+            proposal_cfg (mmcv.Config): Test / postprocessing configuration,
+                if None, test_cfg would be used
+
+        Returns:
+            tuple:
+                losses: (dict[str, Tensor]): A dictionary of loss components.
+                proposal_list (list[Tensor]): Proposals of each image.
+        """
+        outs = self(x)
+        if gt_labels is None:
+            loss_inputs = outs + (gt_bboxes, img_metas)
+        else:
+            loss_inputs = outs + (gt_bboxes, gt_labels, img_metas)
+        losses = self.loss_2(*loss_inputs,semantic_pred, gt_bboxes_ignore=gt_bboxes_ignore)
+        if proposal_cfg is None:
+            return losses
+        else:
+            proposal_list = self.get_bboxes(*outs, img_metas, cfg=proposal_cfg)
+            return losses, proposal_list
+        
 
     def forward_train(self,
                       x,
