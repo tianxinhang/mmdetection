c4ec8bd33d2f37706dfe9390777be848f61450dd
tianxinhang <54171139+tianxinhang@users.noreply.github.com>
2020-10-06 03:11:19 +0800

    Update coco_instance.py

--- configs/_base_/datasets/coco_instance.py
+++ configs/_base_/datasets/coco_instance.py
@@ -1,5 +1,5 @@
 dataset_type = 'CocoDataset'
-data_root = 'data/coco/'
+data_root = '/input/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
@@ -33,16 +33,16 @@ data = dict(
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2017.json',
-        img_prefix=data_root + 'train2017/',
+        img_prefix=data_root + 'pic/train2017/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        img_prefix=data_root + 'pic/val2017/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        img_prefix=data_root + 'pic/val2017/',
         pipeline=test_pipeline))
 evaluation = dict(metric=['bbox', 'segm'])
